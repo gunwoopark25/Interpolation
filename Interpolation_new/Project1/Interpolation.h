5b56ee5f7ccae39dcf2cf42f1e3f3dadc83b6868
DESKTOP-3LTCF83\Parkgeonwoo <gunwoo0122@mail.ulsan.ac.kr>
2026-03-31 18:59:13 +0900

    새로운 버전으로 다시 판다

---
+++ Interpolation_new/Project1/Interpolation.h
@@ -0,0 +1,16 @@
+#pragma once
+class Interpolation
+{
+public:
+	/*--- Variable ---*/
+	int Degree;
+	int Parameter; 
+	int R;
+	int Number_POC;
+	
+	/*--- 2D ---*/
+
+	/*--- Function ---*/
+	void inputData();
+};
+
